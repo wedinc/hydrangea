ca26ad36bab171dc67927df7adfe2be48f322065
Hiroki Taba <taba@HirokinoMacBook-Pro.local>
2023-09-22 18:06:13 +0900

    install Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-gray-300">
-        Hello world!
-      </h1>
-      <button className="btn">Hello daisyUI</button>
+      <h1 className='text-3xl font-bold underline text-gray-300'>Hello world!</h1>
+      <button className='btn'>Hello daisyUI</button>
     </>
   )
 }
