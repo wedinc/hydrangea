4dcfc33927312c6e8757adc345a4530b77ac542f
Hiroki Taba <taba@HirokinoMacBook-Pro.local>
2023-09-22 17:57:28 +0900

    install daisyUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-gray-300">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-gray-300">
+        Hello world!
+      </h1>
+      <button className="btn">Hello daisyUI</button>
+    </>
   )
 }
