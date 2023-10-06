bdafc6f013d1f844eca0177cab111c821bddf2d5
Brocken <kenta.nakayama@wed.company>
2023-10-06 17:40:54 +0900

    fetch Hackernews

--- pages/caramel/index.tsx
+++ pages/caramel/index.tsx
@@ -1,43 +1,22 @@
 import Link from 'next/link'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function Caramel() {
-  const responses = [
-    {
-      by: 'dhouston',
-      descendants: 71,
-      id: 8863,
-      kids: [
-        8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865,
-        8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870,
-        8980, 8934, 8876,
-      ],
-      score: 111,
-      time: 1175714200,
-      title: 'My YC app: Dropbox - Throw away your USB drive',
-      type: 'story',
-      url: 'http://www.getdropbox.com/u/2/screencast.html',
-    },
-    {
-      by: 'dhouston',
-      descendants: 71,
-      id: 8864,
-      kids: [
-        8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865,
-        8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870,
-        8980, 8934, 8876,
-      ],
-      score: 111,
-      time: 1175714200,
-      title: 'kaitokosuge',
-      type: 'story',
-      url: 'http://www.getdropbox.com/u/2/screencast.html',
-    },
-  ]
+  const [ids, setIds] = useState<number[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
+      const data = await res.json()
+      setIds(data)
+    })()
+  }, [])
+
   const List = () => {
     return (
       <ul>
-        {responses.map((response) => (
-          <li>{response.title}</li>
+        {ids.map((id) => (
+          <li>{id}</li>
         ))}
       </ul>
     )
