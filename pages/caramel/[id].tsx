096cb9ce6f785dec0430f4dc2fc5ac0d3e5fb946
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-27 17:33:39 +0900

    feat: get detaul page props

--- pages/caramel/[id].tsx
+++ pages/caramel/[id].tsx
@@ -1,8 +1,29 @@
+import Card, { CardProps } from '@/components/Card'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Article() {
   const router = useRouter()
   const { id } = router.query
 
-  return <p>Article: {id}</p>
+  const [article, setArticle] = useState<CardProps>()
+
+  useEffect(() => {
+    if (!id) return
+    ;(async () => {
+      const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+      const article = await res.json()
+      setArticle(article)
+    })()
+  }, [])
+
+  return (
+    <div className='bg-[#bc611e] p-8'>
+      {article ? <Card {...article} /> : <div />}
+      <Link href='/caramel' className='btn btn-primary'>
+        Back
+      </Link>
+    </div>
+  )
 }
